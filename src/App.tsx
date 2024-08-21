ab5af146376a95d51b1562a2f9429c6405de92ff
Ezequiel <ezequiellalmeida2003@gmail.com>
2024-08-21 12:10:41 -0300

    Componente Home - CSS

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Home from "./pages/home/Home"
+
+
+function App() {
+  
+  //codigo typescript
+
+  return (
+    //codigo tsx => html + css
+   // <h1> A TURMA É TOP!!</h1>
+    <Home />
+    
+  )
+}
+
+export default App
