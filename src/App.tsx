3944acc9f1d4b73418d3880fe721f7ac1c86b49a
Ezequiel <ezequiellalmeida2003@gmail.com>
2024-08-24 15:14:53 -0300

    Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/home/Home"
 
-
 function App() {
-  
-  //codigo typescript
-
+  // Código TypeScript
   return (
-    //codigo tsx => html + css
-   // <h1> A TURMA É TOP!!</h1>
-    <Home />
-    
+    // Código TSX => HTML + CSS
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={ <Home />}/>
+          <Route path="/home" element={ <Home />}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   )
 }
-
 export default App
