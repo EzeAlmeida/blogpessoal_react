6a72bd5696f7c47b4054c42a4239717302a9bead
Ezequiel <ezequiellalmeida2003@gmail.com>
2024-08-31 11:30:55 -0300

    Final

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,26 +1,41 @@
-import { Link } from "react-router-dom"
+import React, { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../contexts/AuthContext'
+
+
 
 function Navbar() {
-    return (
-        <>
-            <div className='w-full flex justify-center py-4
-            			   bg-indigo-900 text-white'>
-            
-                <div className="container flex justify-between text-lg">
-                
-                <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
+  let navigate = useNavigate()
 
-                    <div className='flex gap-4'>
-                        Postagens
-                        Temas
-                        Cadastrar tema
-                        Perfil
-                        Sair
-                    </div>
-                </div>
+  const { usuario, handleLogout } = useContext(AuthContext)
+
+  function logout() {
+      handleLogout()
+      alert('Usuário deslogado com sucesso')
+      navigate('/login')
+  }
+
+  let navbarComponent
+  
+
+  return (
+    <>
+     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+          <div className="container flex justify-between text-lg">
+          <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
+
+            <div className='flex gap-4'>
+            <div className='hover:underline'>Postagens</div>
+            <Link to='/temas' className='hover:underline'>Temas</Link>
+              <div className='hover:underline'>Cadastrar tema</div>
+              <div className='hover:underline'>Perfil</div>
+              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+             
             </div>
-        </>
-    )
+          </div>
+        </div>
+    </>
+  )
 }
 
 export default Navbar
